8d8649605d695eb83e1c13f71f014143965d2ad4
Oussama <gharianioussama24@gmail.com>
2024-08-31 13:30:55 +0100

    docker composer

---
+++ src/main/java/com/example/docker_composer/DockerComposerApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposerApplication.class, args);
+	}
+
+}
